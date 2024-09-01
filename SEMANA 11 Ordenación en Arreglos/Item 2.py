b2091cd238d15caa238a7f323cf725e5acceda3a
loreuea <jl.tonatol@uea.edu.ec>
2024-08-31 19:27:59 -0500

    SEMANA 11_TAREA

--- SEMANA 11 Ordenación en Arreglos/Item 2.py
+++ SEMANA 11 Ordenación en Arreglos/Item 2.py
@@ -4,3 +4,22 @@ matriz = [
     [17, 2, 16],
     [13, 4,68]
 ]
+print("Matriz original")
+print(matriz)
+
+# Método de ordenamiento buble_sort
+def bubble_sort(fila):
+    # algoritmo buble_sort
+    n = len(fila)
+    for i in range(n):
+        for j in range(n-1, i, -1):
+            if fila[j] < fila[j-1]: # Se ubica el signo menor que (<) con el fin de ordenar ascendete como lo requiere la tarea
+                fila[j], fila[j-1] = fila[j-1], fila[j]
+
+# Ordenar la primera las filas de la matriz
+bubble_sort(matriz[1])
+
+print("Matriz con la primera fila ordenada")
+print(matriz)
+
+print(matriz)
